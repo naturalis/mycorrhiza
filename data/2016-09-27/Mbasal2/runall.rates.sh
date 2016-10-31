c3b7cd2311df8f9ea2083a80cf6d3a8e0acb102d
Rutger Vos <rutgeraldo@gmail.com>
2016-10-31 14:18:52 +0100

    now invokes new BayesTraits version 3.0

--- data/2016-09-27/Mbasal2/runall.rates.sh
+++ data/2016-09-27/Mbasal2/runall.rates.sh
@@ -2,15 +2,16 @@
 TREE=Mbasal2.dnd.nex.btin.nex
 DATA=TableS1.tsv
 RATES="qDC qBA qEI qFK qGH"
+BT=BT3.0-OSX-openMP
 for RATE in $RATES; do
-	BayesTraitsV2_OpenMP_Quad $TREE $DATA < restrictions.$RATE.0.txt
+	$BT $TREE $DATA < restrictions.$RATE.0.txt
 	mv TableS1.tsv.log.txt TableS1.tsv.log.$RATE.0.txt
 	mv TableS1.tsv.log.txt.Stones.txt TableS1.tsv.log.txt.Stones.$RATE.0.txt
 done
 
-STATES="A B C D"
-for STATE in $STATES; do
-	BayesTraitsV2_OpenMP_Quad $TREE $DATA < restrictions.root.$STATE.txt
-	mv TableS1.tsv.log.txt TableS1.tsv.log.root.$STATE.txt
-	mv TableS1.tsv.log.txt.Stones.txt TableS1.tsv.log.txt.Stones.root.$STATE.txt
-done
+#STATES="A B C D"
+#for STATE in $STATES; do
+#	BayesTraitsV2_OpenMP_Quad $TREE $DATA < restrictions.root.$STATE.txt
+#	mv TableS1.tsv.log.txt TableS1.tsv.log.root.$STATE.txt
+#	mv TableS1.tsv.log.txt.Stones.txt TableS1.tsv.log.txt.Stones.root.$STATE.txt
+#done
