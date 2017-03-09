2f1dd83c418967cf69c883163288adfa30a21310
Rutger Vos <rutgeraldo@gmail.com>
2017-03-09 14:44:26 +0100

    now moves results in run folder

--- data/2017-03-06/ATxMB/run1.sh
+++ data/2017-03-06/ATxMB/run1.sh
@@ -4,5 +4,5 @@ cp Tree.nex.bt.nex run1
 cp ../HostFungusAssociations.txt.tsv run1
 cd run1
 BT3.0-OSX-openMP Tree.nex.bt.nex ../HostFungusAssociations.txt.tsv < ../unconstrained.txt
-mv ../HostFungusAssociations.txt.tsv.log.txt ../HostFungusAssociations.txt.unconstrained.log
-mv ../HostFungusAssociations.txt.tsv.log.txt.Stones.txt ../HostFungusAssociations.txt.unconstrained.Stones.txt
+mv ../HostFungusAssociations.txt.tsv.log.txt HostFungusAssociations.txt.unconstrained.log
+mv ../HostFungusAssociations.txt.tsv.log.txt.Stones.txt HostFungusAssociations.txt.unconstrained.Stones.txt
